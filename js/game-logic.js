6a120332f4b3f2306eca135303dd71c973619071
JakeOlst <jstainthorpe123@gmail.com>
2024-12-07 21:37:21 +0000

    Fixed Logic for getting words; Added function to get specific word.

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,21 +1,64 @@
-let wordList = [];
+const fs = require('fs').promises;
+const path = require('path');
+
+let wordList, wordLetters, lettersGuessed, guessWord = [];
 let word = "";
 let attempts = 5;
 
-try {
-  fetch("../data/words.json")
-    .then((response) => response.json())
-    .then((words) => {
-      wordList = words;
-    });
-} catch (error) {}
-
-function getWordList() {
-  fetch("../data/words.json")
-    .then((response) => response.json())
-    .then((words) => {
-        wordList = words;
-    });
+async function getWordList() {
+    try {
+        const filePath = path.resolve(__dirname, '../data/words.json');
+        const fileContents = await fs.readFile(filePath, 'utf8');
+        return JSON.parse(fileContents);
+    } catch (error) {
+        console.error("Failed to load word list:", error);
+        throw error;
+    }
 }
 
-function startGame() {}
+async function getRandomWord() {
+    word = wordList[Math.floor(Math.random() * wordList.length)];
+    wordLetters = word.split('');
+    guessWord  = Array(word.length).fill('_');
+    return word;
+}
+
+async function configureGame() {
+    let attempts = 5;
+}
+
+async function startGame() {
+    try {
+        wordList = await getWordList();
+        word = await getRandomWord();
+        console.log("Word List:", wordList);
+        console.log("Random Word:", word);
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+}
+
+function checkLetter(letter) {
+    if (wordLetters.contains(letter)) {
+        for (let i = 0; i < wordLetters.length; i++) {
+            if (wordLetters[i] === letter) {
+                guessWord[i] = letter;
+            }
+        }
+    } else {
+        console.log("Word does not include the letter '"+letter+"'");
+    }
+}
+
+async function makeGuess(letter) {
+    if (!lettersGuessed.contains(letter)) {
+        checkLetter()
+    }
+}
+
+try {
+    configureGame();
+    startGame();
+} catch (error) {
+
+}
